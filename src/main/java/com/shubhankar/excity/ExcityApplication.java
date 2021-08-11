55592781e41ce6e075c5b885470a42e0547f6ab7
shubhankarmane <onlinestudysam@gmail.com>
2021-08-11 16:08:28 +0530

    Check demo route

---
+++ src/main/java/com/shubhankar/excity/ExcityApplication.java
@@ -0,0 +1,13 @@
+package com.shubhankar.excity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubhankar/excity/ExcityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhankar.excity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
